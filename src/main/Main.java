0db40228527af653cc82174e1758501003a20935
urosrolovic <uros.rolovic@gmail.com>
2020-07-01 20:32:28 +0200

    M

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("csoooo");
+		String list = "/";
+		int n = 10;
+		for (int i = 0; i < n; i++) {
+			System.out.println(list);
+			for (int j = n; j > i; j--) {
+				System.out.println(" \n");
+			}
+		}
 
 	}
 
